2d6acfa20de7ad153ac10ea4f0ed80496b470c32
236555 <236555@edu.p.lodz.pl>
2022-04-04 23:06:17 +0200

    Implementacja warstwy Danych, częsciowo Logiki

---
+++ Projekt0/Dane/Ball.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dane
+{
+    internal class Ball
+    {
+        float x;
+        float y;
+        float size;
+
+        float x_speed;
+        float y_speed;
+
+        internal Ball(float x, float y, float size) : this(x, y, size, 0, 0)
+        {
+
+        }
+
+        internal Ball(float x, float y, float size, float x_speed, float y_speed)
+        {
+            this.x = x;
+            this.y = y;
+            this.size = size;
+            this.x_speed = x_speed;
+            this.y_speed = y_speed;
+        }
+    }
+}
