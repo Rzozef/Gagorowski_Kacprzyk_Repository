d71536eedbbafccd56607e4eb5da94bdbd74203f
236555 <236555@edu.p.lodz.pl>
2022-04-21 21:30:35 +0200

    Dodanie API dla warstwy danych

---
+++ Projekt0/Dane/DaneApi.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dane
+{
+    public abstract class DaneAbstractApi
+    {
+        public static DaneAbstractApi CreateApi()
+        {
+            return new DaneApi();
+        }
+    }
+    internal class DaneApi : DaneAbstractApi
+    {
+
+    }
+}
