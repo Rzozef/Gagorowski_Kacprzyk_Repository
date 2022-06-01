41b418971875c671cc1ab32e21443a9ea0d1c5eb
Piotr Gągorowski <236533@edu.p.lodz.pl>
2022-06-01 16:15:50 +0200

    testy przechodzą

---
+++ Projekt0/DaneTests/DataWriterTests.cs
@@ -0,0 +1,22 @@
+﻿using System.IO;
+using Dane;
+using NUnit.Framework;
+
+namespace DaneTests;
+
+public class DataWriterTests
+{
+    private DataWriter writer;
+    [SetUp]
+    public void Setup()
+    {
+        writer = new DataWriter("testDirectory", "testFile");
+    }
+
+    [Test]
+    public void WriteDataTests()
+    {
+        writer.WriteBallsPosition(Stream.Null);
+        Assert.IsNotEmpty(Directory.GetCurrentDirectory() + "/testDirectory" + "/testFile");
+    }
+}
---
+++ Projekt0/LogicTests/LogikaApiTests.cs
@@ -0,0 +1,26 @@
+using System.Numerics;
+using Dane;
+using NUnit.Framework;
+using Logika;
+using BallAbstract = Dane.BallAbstract;
+
+namespace LogicTests;
+
+public class Tests
+{
+    private LogikaAbstractApi api;
+    private BallAbstract ball;
+    [SetUp]
+    public void Setup()
+    {
+        api = LogikaAbstractApi.CreateApi(20, 20);
+        ball = BallAbstract.CreateBall(1, 1, 5, 5,new Vector2(5, 5), DaneAbstractApi.CreateApi(20, 20));
+    }
+
+    [Test]
+    public void GetCollidingBallsTests()
+    {
+        api.CreateBalls(400);
+        Assert.Greater(api.GetCollidingBalls(ball).Count, 0);
+    }
+}
